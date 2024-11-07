721d314fb24b03c1aea905165780e4303e3bf0cb
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-11-06 22:16:36 -0300

    [FEAT]: menu function

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 
-def menu():
-    print("\n" + "="*25)
-    print("[CALCULADORA VALOR/HORA]")
-    print("="*25)
-
-    option = int(input('''
+def menu(option):
+    while option != 5:
+        print(
+'''
 Escolha uma opção:
 1) Registrar despesas mensais
 2) Definir meta salarial
 3) Definir valor geral para imprevistos
 4) Definir meta de horas a trabalhar
-5) Sair\n
-'''))
-    return option
+5) Sair
+'''
+        )
+        option = int(input())
+    return
 
 if __name__ == '__main__':
-    option = menu()
-    while option != 5:
-        menu()
+    option = 0
+
+    print("\n" + "="*25)
+    print("[CALCULADORA VALOR/HORA]")
+    print("="*25)
+
+    menu(option)
