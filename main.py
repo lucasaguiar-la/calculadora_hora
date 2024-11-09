b2818f96e17de98e13a4c150b49edb649617b8ad
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-11-08 21:38:33 -0300

    [FEAT]: Operation: add_expenses

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 global option
-global expense_list
+global expenses
 
 class Operations:
-    def add_expeses():
-        expense_name = str(input("Digite um nome para sua despesa: "))
-        expense_value = float(input("Qual o valor dessa despesa? "))
-        test_list = dict(expense_name, expense_value)
-        return test_list 
+    expenses = {}
 
-def menu(option = 0, expense_list=[]):
-    while option != 5:
+    @staticmethod
+    def add_expenses():
+        expense_name = str(input("\nDigite um nome para sua despesa: "))
+        expense_value = float(input("Qual o valor dessa despesa? "))
+
+        print(f"\nDespesa: {expense_name}\nValor: R$ {expense_value:.2f}")
+        answer = str(input("Deseja adicionar essa despesa? [S/N] \n"))
+        if answer.upper() == "S":
+            Operations.expenses[expense_name] = expense_value
+        elif answer.upper() == "N":
+            return
+        else:
+            print("Opção inválida!")
+            Operations.add_expenses()
+
+def menu(option = 0):
+    while option != 6:
         print(
 '''
 Escolha uma opção:
@@ -17,21 +28,23 @@ Escolha uma opção:
 2) Definir meta salarial
 3) Definir valor geral para imprevistos
 4) Definir meta de horas a trabalhar
-5) Sair
+5) Visualizar despesas mensais
+6) Sair
 '''
         )
         option = int(input())
         match(option):
             case 1:
-                print("Opção 1")
-                expense_list.append(Operations.add_expeses())
-                print(expense_list)
+                Operations.add_expenses()
             case 2:
                 print("Opção 2")
             case 3:
                 print("Opção 3")
             case 4:
                 print("Opção 4")
+            case 5:
+                for name, value in Operations.expenses.items():
+                    print(f"\nDespesa: {name}\nValor: R${value:.2f}")
     return
 
 if __name__ == '__main__':
