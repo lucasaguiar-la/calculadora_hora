ecc7e0a49c8d711c5927c5b34a0093dbeb6159f2
Lucas Aguiar <lucas.aguiar.ads@gmail.com>
2024-11-09 09:15:59 -0300

    [FEAT]: Operations: add_events

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 global option
 global expenses
 global salary
+global events
 
 class Operations:
     expenses = {}
     salary = ""
+    events = {}
 
     @staticmethod
     def add_expenses():
@@ -28,6 +30,21 @@ class Operations:
         Operations.salary = salary_value
         print(f"Meta salarial atualizada para R$ {salary_value}")
 
+    @staticmethod
+    def add_events():
+        event_name = str(input("\nAdicione uma tag para esse emprevisto (ex.: Despesas médicas, Oficina etc...): "))
+        event_value = float(input("Qual o valor dessa tag? "))
+
+        print(f"\nTag: {event_name}\nValor: R$ {event_value:.2f}")
+        answer = str(input("Deseja adicionar essa despesa? [S/N] \n"))
+        if answer.upper() == 'S':
+            Operations.events[event_name] = event_value
+        elif answer.upper() == 'N':
+            return
+        else:
+            print("Opção inválida!")
+            Operations.add_events()
+
 def menu(option = 0):
     while option != 6:
         print(
@@ -37,7 +54,7 @@ Escolha uma opção:
 2) Definir meta salarial
 3) Definir valor geral para imprevistos
 4) Definir meta de horas a trabalhar
-5) Visualizar despesas mensais
+5) Visualizar
 6) Sair
 '''
         )
@@ -48,12 +65,16 @@ Escolha uma opção:
             case 2:
                 Operations.add_salary()
             case 3:
-                print("Opção 3")
+                Operations.add_events()
             case 4:
                 print("Opção 4")
             case 5:
+                print("\n[DESPESAS]")
                 for name, value in Operations.expenses.items():
                     print(f"\nDespesa: {name}\nValor: R${value:.2f}")
+                print("\n[IMPREVISTOS]")
+                for name, value in Operations.events.items():
+                    print(f"\nTag: {name}\nValor: R${value:.2f}")
     return
 
 if __name__ == '__main__':
